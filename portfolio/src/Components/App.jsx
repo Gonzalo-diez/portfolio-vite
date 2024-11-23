4d43644e59928e20fe6438d4866b5b758d34a677
Gonzalo <gonzalodiez97@gmail.com>
2024-11-22 23:16:51 -0300

    Arreglos de estilo

--- portfolio/src/Components/App.jsx
+++ portfolio/src/Components/App.jsx
@@ -1,38 +1,54 @@
-import React, { useState, useEffect, Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-import Layout from './Footer/Layout';
-import Menu from './Menu/Menu';
-import './CSS/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Layout from "./Footer/Layout";
+import Menu from "./Menu/Menu";
+import "./CSS/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Lazy load pages
-const Portfolio = lazy(() => import('../Pages/Portfolio/Portfolio'));
-const Login = lazy(() => import('../Pages/Login/Login'));
-const AddWork = lazy(() => import('../Pages/Works/Add/AddWork'));
-const UpdateWork = lazy(() => import('../Pages/Works/Update/UpdateWork'));
-const DeleteWork = lazy(() => import('../Pages/Works/Delete/DeleteWork'));
-const AddHardSkill = lazy(() => import('../Pages/Skills/Hard/Add/AddHardSkill'));
-const UpdateHardSkill = lazy(() => import('../Pages/Skills/Hard/Update/UpdateHardSkill'));
-const DeleteHardSkill = lazy(() => import('../Pages/Skills/Hard/Delete/DeleteHardSkill'));
-const AddSoftSkill = lazy(() => import('../Pages/Skills/Soft/Add/AddSoftSkill'));
-const UpdateSoftSkill = lazy(() => import('../Pages/Skills/Soft/Update/UpdateSoftSkill'));
-const DeleteSoftSkill = lazy(() => import('../Pages/Skills/Soft/Delete/DeleteSoftSkill'));
-const AddEducation = lazy(() => import('../Pages/Education/Add/AddEducation'));
-const UpdateEducation = lazy(() => import('../Pages/Education/Update/UpdateEducation'));
-const DeleteEducation = lazy(() => import('../Pages/Education/Delete/DeleteEducation'));
+const Portfolio = lazy(() => import("../Pages/Portfolio/Portfolio"));
+const Login = lazy(() => import("../Pages/Login/Login"));
+const AddWork = lazy(() => import("../Pages/Works/Add/AddWork"));
+const UpdateWork = lazy(() => import("../Pages/Works/Update/UpdateWork"));
+const DeleteWork = lazy(() => import("../Pages/Works/Delete/DeleteWork"));
+const AddHardSkill = lazy(() =>
+  import("../Pages/Skills/Hard/Add/AddHardSkill")
+);
+const UpdateHardSkill = lazy(() =>
+  import("../Pages/Skills/Hard/Update/UpdateHardSkill")
+);
+const DeleteHardSkill = lazy(() =>
+  import("../Pages/Skills/Hard/Delete/DeleteHardSkill")
+);
+const AddSoftSkill = lazy(() =>
+  import("../Pages/Skills/Soft/Add/AddSoftSkill")
+);
+const UpdateSoftSkill = lazy(() =>
+  import("../Pages/Skills/Soft/Update/UpdateSoftSkill")
+);
+const DeleteSoftSkill = lazy(() =>
+  import("../Pages/Skills/Soft/Delete/DeleteSoftSkill")
+);
+const AddEducation = lazy(() => import("../Pages/Education/Add/AddEducation"));
+const UpdateEducation = lazy(() =>
+  import("../Pages/Education/Update/UpdateEducation")
+);
+const DeleteEducation = lazy(() =>
+  import("../Pages/Education/Delete/DeleteEducation")
+);
 
 function App() {
   const token = localStorage.getItem("jwtToken");
   const userId = localStorage.getItem("userId");
 
-  const [language, setLanguage] = useState('es');
+  const [language, setLanguage] = useState("es");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
 
   const changeLanguage = (newLanguage) => {
     setLanguage(newLanguage);
-  }
+  };
 
   useEffect(() => {
     const fetchAllData = async () => {
@@ -54,39 +70,105 @@ function App() {
     fetchAllData();
   }, []);
 
-  if (loading) {
-    return (
-      <div className="loading-screen">
-        <span className="loader"></span>
-      </div>
-    )
-  }
-
   return (
     <>
-      <Menu changeLanguage={changeLanguage} language={language} token={token} userId={userId} />
+      <Menu
+        changeLanguage={changeLanguage}
+        language={language}
+        token={token}
+        userId={userId}
+      />
       <Layout>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <div className="loading-screen">
+              <span className="loader"></span>
+            </div>
+          }
+        >
           <Routes>
-            <Route path='/' element={<Portfolio language={language} />} />
-            <Route path='/user/login' element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-            <Route path='/works/protected/addWork' element={<AddWork token={token} isAuthenticated={isAuthenticated} userId={userId} />} />
-            <Route path='/works/protected/updateWork/:id' element={<UpdateWork token={token} userId={userId} />} />
-            <Route path='/works/protected/deleteWork/:id' element={<DeleteWork token={token} userId={userId} />} />
-            <Route path='/hardSkills/protected/addHardSkill' element={<AddHardSkill token={token} isAuthenticated={isAuthenticated} userId={userId} />} />
-            <Route path='/hardSkills/protected/updateHardSkill/:id' element={<UpdateHardSkill token={token} userId={userId} />} />
-            <Route path='/hardSkills/protected/deleteHardSkill/:id' element={<DeleteHardSkill token={token} userId={userId} />} />
-            <Route path='/softSkills/protected/addSoftSkill' element={<AddSoftSkill token={token} isAuthenticated={isAuthenticated} userId={userId} />} />
-            <Route path='/softSkills/protected/updateSoftSkill/:id' element={<UpdateSoftSkill token={token} userId={userId} />} />
-            <Route path='/softSkills/protected/deleteSoftSkill/:id' element={<DeleteSoftSkill token={token} userId={userId} />} />
-            <Route path='/educations/protected/addEducation' element={<AddEducation token={token} isAuthenticated={isAuthenticated} userId={userId} />} />
-            <Route path='/educations/protected/updateEducation/:id' element={<UpdateEducation token={token} userId={userId} />} />
-            <Route path='/educations/protected/deleteEducation/:id' element={<DeleteEducation token={token} userId={userId} />} />
+            <Route path="/" element={<Portfolio language={language} />} />
+            <Route
+              path="/user/login"
+              element={<Login setIsAuthenticated={setIsAuthenticated} />}
+            />
+            <Route
+              path="/works/protected/addWork"
+              element={
+                <AddWork
+                  token={token}
+                  isAuthenticated={isAuthenticated}
+                  userId={userId}
+                />
+              }
+            />
+            <Route
+              path="/works/protected/updateWork/:id"
+              element={<UpdateWork token={token} userId={userId} />}
+            />
+            <Route
+              path="/works/protected/deleteWork/:id"
+              element={<DeleteWork token={token} userId={userId} />}
+            />
+            <Route
+              path="/hardSkills/protected/addHardSkill"
+              element={
+                <AddHardSkill
+                  token={token}
+                  isAuthenticated={isAuthenticated}
+                  userId={userId}
+                />
+              }
+            />
+            <Route
+              path="/hardSkills/protected/updateHardSkill/:id"
+              element={<UpdateHardSkill token={token} userId={userId} />}
+            />
+            <Route
+              path="/hardSkills/protected/deleteHardSkill/:id"
+              element={<DeleteHardSkill token={token} userId={userId} />}
+            />
+            <Route
+              path="/softSkills/protected/addSoftSkill"
+              element={
+                <AddSoftSkill
+                  token={token}
+                  isAuthenticated={isAuthenticated}
+                  userId={userId}
+                />
+              }
+            />
+            <Route
+              path="/softSkills/protected/updateSoftSkill/:id"
+              element={<UpdateSoftSkill token={token} userId={userId} />}
+            />
+            <Route
+              path="/softSkills/protected/deleteSoftSkill/:id"
+              element={<DeleteSoftSkill token={token} userId={userId} />}
+            />
+            <Route
+              path="/educations/protected/addEducation"
+              element={
+                <AddEducation
+                  token={token}
+                  isAuthenticated={isAuthenticated}
+                  userId={userId}
+                />
+              }
+            />
+            <Route
+              path="/educations/protected/updateEducation/:id"
+              element={<UpdateEducation token={token} userId={userId} />}
+            />
+            <Route
+              path="/educations/protected/deleteEducation/:id"
+              element={<DeleteEducation token={token} userId={userId} />}
+            />
           </Routes>
         </Suspense>
       </Layout>
     </>
-  )
+  );
 }
 
 export default App;
