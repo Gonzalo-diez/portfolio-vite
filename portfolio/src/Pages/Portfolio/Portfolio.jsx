5518fc37b10abdf52c2594c6735dd9b71ac81230
Gonzalo <gonzalodiez97@gmail.com>
2024-11-27 14:57:04 -0300

    Arreglos del portfolio

--- portfolio/src/Pages/Portfolio/Portfolio.jsx
+++ portfolio/src/Pages/Portfolio/Portfolio.jsx
@@ -3,8 +3,8 @@ import Home from "../../Sections/Home/Home";
 import About from "../../Sections/About/About";
 import Curriculum from "../../Sections/CV/Curriculum";
 import Contact from "../../Sections/Contact/Contact";
-import Education from "../../Sections/Education";
-import Skills from "../../Sections/Skills";
+import Education from "../../Sections/Education/Education";
+import Skills from "../../Sections/Skills/Skills";
 
 const LazyWorks = React.lazy(() => import("../../Sections/Works/Works"));
 
