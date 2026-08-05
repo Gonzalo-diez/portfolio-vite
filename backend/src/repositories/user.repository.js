a5f20ff70f70a8b6e721f08948332e3348b3e8c1
Gonzalo <gonzalodiez97@gmail.com>
2026-08-05 18:41:37 -0300

    Rewrite completo con Tailwind + shadcn 3

--- backend/src/repositories/user.repository.js
+++
@@ -1,9 +0,0 @@
-import User from "../dao/models/user.js";
-
-const userRepository = {
-    findById: async (userId) => {
-        return await User.findById(userId);
-    }
-}
-
-export default userRepository;
